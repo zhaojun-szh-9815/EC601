a5d6261bbe8e9b00b6641c91f3fa2efcb0577db7
朝菌君桾覠頵 <55321300+zhaojun-szh-9815@users.noreply.github.com>
2021-10-28 00:09:04 -0400

    Create Test_Updated_Media_Analyzer.py

---
+++ Media_Analyzer/Test_Updated_Media_Analyzer.py
@@ -0,0 +1,31 @@
+import Media_Analyse
+import pytest
+import tweepy
+
+consumer_key = "******"
+consumer_secret = "******"
+access_key = "******"
+access_secret = "******"
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_key, access_secret)
+api = tweepy.API(auth)
+
+def test_get_keyword_input(monkeypatch):
+    monkeypatch.setattr('builtins.input', lambda _:"Genshin,Boston University")
+    keywords_input = input('give me keywords (use comma to split):')
+    get_keywords = Media_Analyse.get_keywords_from_input(keywords_input)
+    assert get_keywords == ['Genshin','Boston University']
+
+def test_get_keyword_param():
+    get_keywords = Media_Analyse.get_keywords_from_param()
+    assert get_keywords == ['Test_Media_Analyse.py']
+
+def test_get_tweets():
+    keywords = 'Genshin'
+    get_tweets = Media_Analyse.get_tweets(keywords,api)
+    assert get_tweets == None
+
+def test_process_time():
+    time_process = Media_Analyse.main()
+    assert time_process <= 0.5
